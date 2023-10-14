78a154146002d57e46303d1930ed063f0b7eaa00
Chris Wagner <chriswagner@wagnerchKMD6R.vmware.com>
2023-10-13 19:53:56 -0500

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.sendFile('/Users/chriswagner/Desktop/JS WD/notesApp/frontend/index.js')
+})
+
+app.listen(6000, function () {
+    console.log('Server started on port 6000...')
+})
