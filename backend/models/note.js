9af9f69c54012e9cc2e72f01cc985f3e6aa557c8
Chris Wagner <chriswagner@wagnerchKMD6R.vmware.com>
2023-11-02 16:59:03 -0500

    Add model and schema for DB

---
+++ backend/models/note.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+    title: {
+        type: String,
+        required: false
+    },
+    content: {
+        type: String,
+        required: false
+    }
+},
+    { timestamps: true });
+
+const Note = mongoose.model('Note', noteSchema);
+
+module.exports = Note;
+
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
@@ -8,9 +9,17 @@ app.use(bodyParser.json());
 
 // connect to mongoDB
 const adminName = process.env.MONGODBUSERNAME
-const password = process.env.MONGODBPASSWORD
+const password = encodeURIComponent(process.env.MONGODBPASSWORD)
 const dbURI = `mongodb+srv://${adminName}:${password}@cluster0.epj1x.mongodb.net/?retryWrites=true&w=majority`
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(dbURI)
+    .then(res => {
+        console.log('connected to database...')
+        app.listen(6000, function () {
+            console.log('Server started on port 6000...')
+        })
+    })
+    .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.sendFile('/Users/chriswagner/Desktop/JS WD/notesApp/frontend/src/index.js')
@@ -18,15 +27,11 @@ app.get('/', (req, res) => {
 
 // app.post('/')
 
-app.listen(6000, function () {
-    console.log('Server started on port 6000...')
-})
+// app.listen(6000, function () {
+//     console.log('Server started on port 6000...')
+// })
 
 // https://www.linkedin.com/advice/0/how-do-you-securely-store-manage-user#:~:text=Use%20hashing%20and%20salting%20for%20passwords&text=You%20should%20use%20a%20secure,as%20MD5%20or%20SHA%2D1.
 // https://www.youtube.com/watch?v=bxsemcrY4gQ
 // https://stackoverflow.com/questions/60498948/securely-use-connection-string-of-mongoatlas-in-node-js-to-connect-to-mongodb
 // encrypt and decrypt connection string when using
-
-
-// TODO
-// backend node_modules need to be included in .gitignore
