fd5d4014c3ac7e45ef10c13fd03a06e5913e1e06
Chris Wagner <chriswagner@wagnerchKMD6R.vmware.com>
2023-11-17 15:02:02 -0600

    Various changes

--- frontend/src/components/AddButton.jsx
+++ frontend/src/components/AddButton.jsx
@@ -2,7 +2,6 @@ import '../CSS/AddButton.css'
 
 function AddButton() {
     return (
-        // <button type='button' className='addButton' onClick={props.onClick}>+</button>
         <input type='submit' className='addButton' value='+' />
     )
 }
--- frontend/src/components/Background.jsx
+++ frontend/src/components/Background.jsx
@@ -42,3 +42,5 @@ function Background(props) {
 }
 
 export default Background
+
+// justify-content: space-between space-around space-evenly
